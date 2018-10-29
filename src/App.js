07f12f1b5b0f122dc7a64c0d4fa8135e9968be6d
György Fodor <gfodor85@gmail.com>
2018-10-29 20:33:17 +0100

    -

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './index.sass';
 class App extends Component {
   render() {
     return (
-      <div className="app">
+      <div className="app" style={{backgroundImage: 'images/background.jpg'}}>
         <Nav/> 
       </div>
     );
