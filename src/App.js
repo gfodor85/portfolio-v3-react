53a31045436fb9d372786fda182c7b5482fe8f17
György Fodor <gfodor85@gmail.com>
2018-10-29 16:37:42 +0100

    -

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import about_icon from './images/svg/about_icon.svg';
-import work_icon from './images/svg/work_icon.svg';
-import skills_icon from './images/svg/skills_icon.svg';
-import contact_icon from './images/svg/contact_icon.svg';
+
 import Nav from './components/nav';
 import './index.sass';
 // import {CSSTransition} from "react-transition-group";
