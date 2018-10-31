dd7021edeb67ea133f845d4c41e7bf09002de5b1
György Fodor <gfodor85@gmail.com>
2018-10-31 11:29:38 +0100

    -

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import Nav from './components/nav';
-import './index.sass';
-// import {CSSTransition} from "react-transition-group";
+import About from './components/about';
+import './App.sass';
+
+// import bg from './images/background.jpg'
+// import// import {CSSTransition} from "react-transition-group";
 
 
 class App extends Component {
   render() {
+    var 
+      bg = require('./images/background.jpg'),
+      appStyle = {
+        backgroundImage: "url("+ bg +")",
+        height: '100vh'
+      };
+    
     return (
-      <div className="app">
-        <Nav/> 
+      <div className="app" style ={appStyle}>
+        <Nav/>
+        <About/>
       </div>
     );
   }
--- src/components/about.js
+++ src/components/about.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import about_icon from '../images/svg/about_icon.svg';
+
 
 class About extends Component {
     render () {
       return (
 
-        
+        <div className="panel-container">
+          <div className="panel-content">
+            <div className="panel-icon">
+                <img src={about_icon} className="panel-svg" alt="about-icon" />
+            </div>
+            <p className="abaout-text">
+              Hello,<br />
+                I’m an enthusiastic front-end developer and designer with many years of experience
+                in building and designing clean and fast websites. I am always opened to new ideas
+                and new ways which help me to develop my skills and try to become better and
+                better. In my adventure of development I always learn new methods and technologies
+                which result in cool websites. I have experience in creating a wide range of web
+                projects such as landing pages, eCommerce or WordPress sites that will work and
+                look great across all devices.
+            </p>
+            <div className="title-container">
+              <div className="title-text">About Me</div>
+            </div>
+          </div>
+        </div>
 
       );
     }
