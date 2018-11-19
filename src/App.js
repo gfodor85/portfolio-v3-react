cb80b9ddd124027b5e5d769cd992235f5be15002
György Fodor <gfodor85@gmail.com>
2018-11-19 13:42:51 +0100

    -

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
+import { AnimatedSwitch } from 'react-router-transition';
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 import Nav from "./components/nav/nav";
@@ -22,12 +23,17 @@ class App extends Component {
     return (
       <div className="app" style={appStyle}>
         <Nav />
-        <Switch>
-          <Route exact path="/" component={PanelAbout} />
-          <Route path="/work" component={PanelWork} />
-          <Route path="/skills" component={PanelSkills} />
-          <Route path="/contact" component={PanelContact} />
-        </Switch>
+          <AnimatedSwitch
+          atEnter={{ opacity: 0 }}
+          atLeave={{ opacity: 0 }}
+          atActive={{ opacity: 1 }}
+          className="switch-wrapper">
+
+            <Route exact path="/" component={PanelAbout} />
+            <Route path="/work" component={PanelWork} />
+            <Route path="/skills" component={PanelSkills} />
+            <Route path="/contact" component={PanelContact} />
+          </AnimatedSwitch>
       </div>
     );
   }
--- src/components/panels/panel_about.js
+++ src/components/panels/panel_about.js
@@ -1,32 +1,34 @@
 import React, { Component } from "react";
 import AboutIcon from "../../images/svg/about_icon.svg";
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
 
 
 const PanelAbout = () => {
 
   return (
-      <div className="panel-container">
-        <div className="panel-content">
-          <div className="panel-icon">
-            <img src={AboutIcon} className="panel-svg" alt="about-icon" />
-          </div>
-          <p className="about-text">
-            Hello,
-            <br />
-            I’m an enthusiastic front-end developer and designer with many years
-            of experience in building and designing clean and fast websites. I
-            am always opened to new ideas and new ways which help me to develop
-            my skills and try to become better and better. In my adventure of
-            development I always learn new methods and technologies which result
-            in cool websites. I have experience in creating a wide range of web
-            projects such as landing pages, eCommerce or WordPress sites that
-            will work and look great across all devices.
-          </p>
-          <div className="title-container">
-            <div className="title-text">About Me</div>
-          </div>
-        </div>
-      </div>
+        <div className="panel-container">
+            <div className="panel-content">
+            <div className="panel-icon">
+                <img src={AboutIcon} className="panel-svg" alt="about-icon" />
+            </div>
+            <p className="about-text">
+                Hello,
+                <br />
+                I’m an enthusiastic front-end developer and designer with many years
+                of experience in building and designing clean and fast websites. I
+                am always opened to new ideas and new ways which help me to develop
+                my skills and try to become better and better. In my adventure of
+                development I always learn new methods and technologies which result
+                in cool websites. I have experience in creating a wide range of web
+                projects such as landing pages, eCommerce or WordPress sites that
+                will work and look great across all devices.
+            </p>
+            <div className="title-container">
+                <div className="title-text">About Me</div>
+            </div>
+            </div>
+        </div>  
     );
   }
 
