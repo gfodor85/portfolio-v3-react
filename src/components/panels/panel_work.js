ea5348e15150179eddbaa9e7b86085cc65b51069
György Fodor <gfodor85@gmail.com>
2018-11-26 06:58:33 +0100

    -

--- src/components/panels/panel_work.js
+++ src/components/panels/panel_work.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
 import WorkIcon from "../../images/svg/work_icon.svg";
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faEnvelope, faKey } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "react-animated-slider/build/horizontal.css";
 
+import Slider from "react-animated-slider";
+import WorkContent from "./works_data"
 
-// import Slider from "react-animated-slider";
-
-library.add(faEnvelope, faKey);
-
+library.add(faAngleLeft, faAngleRight);
 
 const PanelWork = () => {
+
   return (
     <div className="panel-container">
       <div className="panel-content">
@@ -18,18 +19,54 @@ const PanelWork = () => {
           <img src={WorkIcon} className="panel-svg" alt="about-icon" />
         </div>
         <div className="work-wrap">
-          <div class="nav" aria-label="Slider buttons" aria-controls="sequence">
+
           
-              <i class="fas fa-chevron-left fa-lg seq-prev" aria-label="Previous" aria-hidden="true"></i>
-              <i class="fas fa-chevron-right fa-lg seq-next" aria-label="Next" aria-hidden="true"></i>
+
+          <Slider 
+            className="slider-wrapper"
+          >
+            {WorkContent.map((item, index) => (
+              <div
+                key={index}
+                className="slider-content"
+                style={{
+                  background: `url('${item.image}') no-repeat center center`
+                }}
+              >
+                <div className="inner">
+                  <h1>{item.title}</h1>
+                  <p>{item.description}</p>
+                  <button>{item.button}</button>
+                </div>
+                <section>
+                  <img src={item.userProfile} alt={item.user} />
+                  <span>
+                    Posted by <strong>{item.user}</strong>
+                  </span>
+                </section>
+              </div>
+            ))}
+          </Slider>
+          <div className="work-nav-wrap">
+            <a className="previousButton">
+              <FontAwesomeIcon
+                className="work-nav-button nextButton"
+                icon="angle-left"
+                size="2x"
+              />
+            </a>
+            <FontAwesomeIcon
+              className="work-nav-button"
+              icon="angle-right"
+              size="2x"
+            />
           </div>
-          <FontAwesomeIcon icon="envelope" />
+        </div>
         <div className="title-container">
           <div className="title-text">Recent Works</div>
         </div>
       </div>
     </div>
-  </div>
   );
 };
 
---
+++ src/components/panels/works_data.js
@@ -0,0 +1,23 @@
+export default [
+    {
+      title: "Vulputate Mollis Ultricies Fermentum Parturient",
+      description:
+        "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.",
+      imageDesktop: "https://i.imgur.com/DCdBXcq.jpg",
+      imagePhone: "https://i.imgur.com/DCdBXcq.jpg"
+    },
+    {
+      title: "Tortor Dapibus Commodo Aenean Quam",
+      description:
+        "Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.",
+      imageDesktop: "https://i.imgur.com/DCdBXcq.jpg",
+      imagePhone: "https://i.imgur.com/DCdBXcq.jpg"
+    },
+    {
+      title: "Phasellus volutpat metus",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.",
+      imageDesktop: "https://i.imgur.com/DCdBXcq.jpg",
+      imagePhone: "https://i.imgur.com/DCdBXcq.jpg"
+    }
+  ];
